211aa54763c9bece56f1d4d62d8382940c908b95
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-01 23:37:38 -0300

    add spring project

---
+++ src/main/java/org/example/linksphere/LinkSphereApplication.java
@@ -0,0 +1,13 @@
+package org.example.linksphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/linksphere/LinkSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.linksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
