95622b8a7c1f0787c8915ee51e52900f4f7a5f74
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-10-03 15:45:17 -0300

    feat: add attributes in responseDTO

--- src/main/java/org/example/linksphere/dto/response/PostResponseDTO.java
+++ src/main/java/org/example/linksphere/dto/response/PostResponseDTO.java
@@ -1,4 +1,13 @@
 package org.example.linksphere.dto.response;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class PostResponseDTO {
+    private String title;
+    private String content;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
 }
